68db19c090fcbd02c96bbbc3e1f62a2f05603e54
Petr Fifka <petr.fifka@tredgate.cz>
2025-02-19 19:09:28 +0100

    Controlling Cypress Functions

--- cypress/e2e/learning/basics/operating_with_cypress_tests.cy.js
+++ cypress/e2e/learning/basics/operating_with_cypress_tests.cy.js
@@ -1,9 +1,13 @@
 // Všechny testy musí být ve složce cypress/e2e
 
 describe("Operating with Cypress Tests", () => {
-  it("Login Tests", () => {
-    cy.visit("https://tredgate.com/pmtool");
-    cy.get("#username");
+  // Only slouží pro vývoj testů a dělá to, že spouští jen testy označené "it.only"
+  // ! Nezapomeňte only smazat před pushováním do repozitáře. Způsobí problémy při běhu testů v cloudu
+  it.only("Login Tests", () => {
+    cy.visit("https://tredgate.com/pmtool"); // Otevře webovou stránku
+    cy.get("#username").type("cypress_zima_2024"); // Vyplnění pole uživatelské jméno
+    cy.get("#password").type("Zima2024Cypress");
+    cy.get(".btn").click(); // Kliknutí na tlačítko
   });
 
   it.skip("Test failure - element not present", () => {
@@ -11,3 +15,14 @@ describe("Operating with Cypress Tests", () => {
     cy.get("#username2");
   });
 });
+
+/*
+
+Cvičení - ztracené heslo (⌛10:00)
+Vytvořte novou složku cypress/e2e/exercises
+Vytvořte v ní nový test soubor “lost_password_tests.cy.js”
+Vytvořte describe “Lost passwords tests”
+Vytvořte it “Lost password test”
+Otevřete pmtool, klikněte forgotten password, vyplňte nejdříve e-mail: test@tredgate.cz, poté username: test a potvrďte.
+
+*/
