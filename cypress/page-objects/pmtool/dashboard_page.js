2420ab9a8c36b35b6106f6b0e7ad880852efca84
Petr Fifka <petrfifk@gmail.com>
2025-03-17 18:47:15 +0100

    Atomic tests - prepare PO

--- cypress/page-objects/pmtool/dashboard_page.js
+++ cypress/page-objects/pmtool/dashboard_page.js
@@ -1,10 +1,36 @@
 import { HeaderSection } from "./common/header_section.js";
-import { ProjectsPage } from "./projects_page.js";
 
 export class DashboardPage extends HeaderSection {
   constructor() {
-    super(); // ? super() - provolání děděného constructoru, je povinný a musí být vždy v constructor() na prvním místě.
+    // ? Parametr path "module=dashboard/dashboard" přidáváme z toho důvodu, abychom v testu mohli např. Dashboard otevřít napřímo: new DashboardPage().visit();
+
+    super("module=dashboard/dashboard"); // ? super() - provolání děděného constructoru, je povinný a musí být vždy v constructor() na prvním místě.
+
     this.welcomePageHeader = "#welcome-page-header";
-    cy.get(this.welcomePageHeader).should("be.visible");
+
+    // ! Assert v constructoru nemůžeme použít, pokud máme funkcionalitu přímého otevření (BasePage.visit)
+    // ! když zavoláme new Dashboard().visit(), tak se nejdříve vyhodnocuje constructor a až potom visit() a to znamená, že před tím než stránku otevřeme už kontrolujeme nějaký assert, což znamená, že assert selže.
+    // cy.get(this.welcomePageHeader).should("be.visible");
+
+    // ? Pokud chci kontrolovat načtení stránky, potom je potřeba vytvořit samostatnou metodu, která se bude ručně provolávat.
+
+    /*
+    Proces využití path v BasePage
+    situace: jsem v projectsPage a chci napřímo otevřít Dashboard page:  
+    new DashboardPage().visit()
+    
+    Kroky:
+     1. Zavolání konstruktoru DashboardPage (jsem stále v Projects)
+     2. V constructoru je assert na viditelnost welcomePageHeader
+     3. Timeout na assert (jsme stále v Projects)
+     4. Až po constructoru by se zavolal visit()
+    */
   }
 }
+
+/*
+
+Cvičení - příprava ProjectsPage ⌛3:00:
+Připravte stejným stylem objekt ProjectsPage.
+
+*/
--- cypress/page-objects/pmtool/lost_password_page.js
+++ cypress/page-objects/pmtool/lost_password_page.js
@@ -1,7 +1,9 @@
+import { BasePage } from "./common/base_page.js";
 import { LoginPage } from "./login_page.js";
 
-export class LostPasswordPage {
+export class LostPasswordPage extends BasePage {
   constructor() {
+    super("module=users/restore_password");
     this.emailInput = ":nth-child(3) > .input-icon > .form-control";
     this.usernameInput = ":nth-child(2) > .input-icon > .form-control";
     this.submitButton = '[type="submit"]';
