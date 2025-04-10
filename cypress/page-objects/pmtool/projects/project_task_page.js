61d3e9775550ed528d13dc580f40da958048a114
Petr Fifka <petr.fifka@tredgate.cz>
2025-04-11 00:40:35 +0200

    ddt files

--- cypress/page-objects/pmtool/projects/project_task_page.js
+++ cypress/page-objects/pmtool/projects/project_task_page.js
@@ -2,6 +2,7 @@
 //page-objects/pmtool/projects/project_task_page.js
 
 import { HeaderSection } from "../common/header_section.js";
+import { ProjectInfoPage } from "./project_info_page.js";
 
 export class ProjectTaskPage extends HeaderSection {
   // ? constructor(projectId="") <== použití výchozí hodnoty pro parametr projectId. Kdokoliv tento objekt zakládá může, ale nemusí poslat projectId. Pokud ho nepošle, parametru projectId se přiřadí hodnota ""
@@ -10,5 +11,11 @@ export class ProjectTaskPage extends HeaderSection {
   // *    new ProjectTaskPage(); // parametru projectId se přiřadí výchozí hodnota ""
   constructor(projectId = "") {
     super(`module=items/items&path=21-${projectId}}/22`);
+    this.projectInfoAnchor = ".navbar-header .navbar-brand";
+  }
+
+  clickProjectInfo() {
+    cy.get(this.projectInfoAnchor).click();
+    return new ProjectInfoPage();
   }
 }
